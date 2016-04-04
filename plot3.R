6e2f58f200a3842a17c5e3f680c2aecd8f2d0c31
Eric Tegenfeldt <ericteg80@gmail.com>
2016-04-03 17:32:29 -0700

    Added files via upload

---
+++ plot3.R
@@ -0,0 +1,33 @@
+##  1. Download the data 
+library(data.table)
+if (!file.exists('.exdata-data-household_power_consumption.zip')) {
+        fileUrl = 'http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+        download.file(fileUrl,'.exdata-data-household_power_consumption.zip', mode = 'wb')
+        unzip(".exdata-data-household_power_consumption.zip", exdir = './')
+}
+##  2. Load the table and then extract the data from the target dates (Feb 1 and 2, 2007) into a dataframe
+##     The second plot only requires the date, time, and Global Power columns (1-3) so the rest are dropped
+##     on load.
+
+col_to_drop <- c(3:6)
+all_rows <- fread("household_power_consumption.txt", header=TRUE, sep = ';', na.strings="?", drop=col_to_drop,data.table=F)
+all_rows$Date <- as.Date(all_rows$Date, format="%d/%m/%Y") 
+plot_data <- all_rows[(all_rows$Date=="2007-02-01") | (all_rows$Date=="2007-02-02"),]
+plot_data <- transform(plot_data, daytime=as.POSIXct(paste(Date,Time)))
+
+##  3. Reset and Construct the plot
+
+if (dev.cur() != 1) 
+{dev.off()}
+
+plot(plot_data$daytime, plot_data$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
+lines(plot_data$daytime,plot_data$Sub_metering_2,col="red")
+lines(plot_data$daytime,plot_data$Sub_metering_3,col="blue")
+legend("topright",
+        col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ","Sub_metering_3  "),
+          lty=c(1,1),lwd=c(1,1))
+
+##  4. Copy the plot to the PNG file
+
+dev.copy(png, file="plot3.png", width = 480, height = 480)
+dev.off()
